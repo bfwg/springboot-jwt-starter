97f08ffe895fad093b06fae54d07c4d9da16cb4c
Fan Jin <fanjin1989@gmail.com>
2018-07-02 20:51:47 -0700

    add external resource folder

---
+++ src/main/java/com/bfwg/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.bfwg.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/files/**")
+                .addResourceLocations("file:/opt/jwt-demo/files/");
+    }
+}
--- src/main/java/com/bfwg/config/WebSecurityConfig.java
+++ src/main/java/com/bfwg/config/WebSecurityConfig.java
@@ -64,6 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(
                         HttpMethod.GET,
+                        "/files/**",
                         "/",
                         "/auth/**",
                         "/webjars/**",
