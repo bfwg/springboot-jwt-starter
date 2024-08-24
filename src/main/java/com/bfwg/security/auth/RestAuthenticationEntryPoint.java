afde2acf1be14d4f07914e4010d2cc93b05ffdc9
mirko <mirko.perillo@gmail.com>
2024-08-24 18:03:21 +0200

    fix formatting

--- src/main/java/com/bfwg/security/auth/RestAuthenticationEntryPoint.java
+++ src/main/java/com/bfwg/security/auth/RestAuthenticationEntryPoint.java
@@ -19,14 +19,14 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-	@Override
-	public void commence(HttpServletRequest request,
-			HttpServletResponse response,
-			AuthenticationException authException) throws IOException {
-		// This is invoked when user tries to access a secured REST resource without
-		// supplying any credentials
-		// We should just send a 401 Unauthorized response because there is no 'login
-		// page' to redirect to
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
-	}
+    @Override
+    public void commence(HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException {
+        // This is invoked when user tries to access a secured REST resource without
+        // supplying any credentials
+        // We should just send a 401 Unauthorized response because there is no 'login
+        // page' to redirect to
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
 }
